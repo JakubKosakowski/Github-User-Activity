6cdc875c92c5b7391e1e246c563bffbb8f440a25
JakubKosakowski <jkosakowski602@gmail.com>
2024-10-14 12:26:13 +0200

    Implement user CLI command

--- app/cli.py
+++ app/cli.py
@@ -1,16 +1,26 @@
-from typing import Optional
+from typing import Optional, List
 
 import typer
 
-from app import __app_name__, __version__
+from app import __app_name__, __version__, api
 
 app = typer.Typer()
+
+def get_api_handler() -> api.APIHandler:
+    return api.APIHandler()
 
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
 
+@app.command()
+def user(
+    username: str = typer.Argument(...)
+) -> List[str]:
+    api_handler = get_api_handler()
+    data = api_handler.get_user_info(username)
+    typer.secho(data, fg=typer.colors.BLUE)
 
 @app.callback()
 def main(
