174ae3bc1909c84e30efdbdadc0813a62e73d022
JakubKosakowski <jkosakowski602@gmail.com>
2024-10-14 12:40:12 +0200

    Create unit test for event CLI command

--- tests/test_github_app.py
+++ tests/test_github_app.py
@@ -30,3 +30,22 @@ def test_user(username: str, expected_public_repos: int, expected_followers: int
     user = api_handler.get_user_info(username)
     assert user['public_repos'] == expected_public_repos
     assert user['followers'] == expected_followers
+
+@pytest.mark.parametrize(
+    "expected_actor_url, expected_repo_name",
+    [
+        pytest.param(
+            "https://api.github.com/users/JakubKosakowski",
+            "JakubKosakowski/Github-User-Activity"
+        )
+    ]
+)
+def test_events(
+        expected_actor_url: str,
+        expected_repo_name: str,
+        ):
+    api_handler = cli.get_api_handler()
+    user = api_handler.get_user_events("JakubKosakowski")
+    latest_event = user[0]
+    assert latest_event['actor']['url'] == expected_actor_url
+    assert latest_event['repo']['name'] == expected_repo_name
