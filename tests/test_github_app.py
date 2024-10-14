d1bac51c3f6d10010090e61c5594931257f72377
JakubKosakowski <jkosakowski602@gmail.com>
2024-10-14 12:24:40 +0200

    Create unit test for user CLI command

--- tests/test_github_app.py
+++ tests/test_github_app.py
@@ -1,3 +1,5 @@
+import pytest
+
 from typer.testing import CliRunner
 
 from app import __app_name__, __version__, cli
@@ -8,3 +10,23 @@ def test_version():
     result = runner.invoke(cli.app, ['--version'])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}" in result.stdout
+
+test_data1 = {
+    "username": 'JakubKosakowski'
+}
+
+@pytest.mark.parametrize(
+    "username, expected_public_repos, expected_followers",
+    [
+        pytest.param(
+            test_data1['username'],
+            15,
+            3
+        )
+    ]
+)
+def test_user(username: str, expected_public_repos: int, expected_followers: int):
+    api_handler = cli.get_api_handler()
+    user = api_handler.get_user_info(username)
+    assert user['public_repos'] == expected_public_repos
+    assert user['followers'] == expected_followers
